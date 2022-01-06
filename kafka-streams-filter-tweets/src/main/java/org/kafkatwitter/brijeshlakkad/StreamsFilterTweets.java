0a9922c8641a3bf5aa629687ff0f2309e3acd4e2
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2022-01-06 16:12:56 -0500

    Kafka Streams: to filter tweets

---
+++ kafka-streams-filter-tweets/src/main/java/org/kafkatwitter/brijeshlakkad/StreamsFilterTweets.java
@@ -0,0 +1,13 @@
+package org.kafkatwitter.brijeshlakkad;
+
+public class StreamsFilterTweets {
+    public static void main(String[] args){
+        // create properties
+
+        // create a topology
+
+        // build the topology
+
+        // start our streams application
+    }
+}
